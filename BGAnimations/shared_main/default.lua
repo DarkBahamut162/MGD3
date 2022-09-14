28ea0b30f9f1c214f1f6d2f9ba8f18ce301831ab
DarkBahamut162 <dark_bahamut162@hotmail.com>
2022-09-14 13:56:16 +0200

    Darken shared_main isn't needed at all

--- BGAnimations/shared_main/default.lua
+++ BGAnimations/shared_main/default.lua
@@ -2,7 +2,4 @@ return Def.ActorFrame{
 	LoadActor("shared_main")..{
 		InitCommand=function(self) self:FullScreen() end
 	}
---	Def.Quad{
---		OnCommand=function(self) self:FullScreen():diffusecolor(Color.Black):diffusealpha(0.6) end
---	}
 }

f4642f07632fc81f747d79f24ef14c2232cb94f9
DarkBahamut162 <dark_bahamut162@hotmail.com>
2024-01-29 08:35:03 +0100

    Use Jackets instead of Backgrounds if available

--- Graphics/MusicWheelItem Song NormalPart/default.lua
+++ Graphics/MusicWheelItem Song NormalPart/default.lua
@@ -2,7 +2,9 @@ return Def.ActorFrame{
 	Def.Sprite{
 		SetMessageCommand=function(self,params)
 			local Song = params.Song
-			if Song and Song:HasBackground() then
+			if Song and Song:HasJacket() then
+				self:LoadBanner(Song:GetJacketPath())
+			elseif Song and Song:HasBackground() then
 				self:LoadBackground(Song:GetBackgroundPath())
 			else
 				self:Load(THEME:GetPathG("Common fallback", "banner"))
